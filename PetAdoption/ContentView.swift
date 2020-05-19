c6b660e2630918759cc6abd1818a6284139a91e6
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-05-19 21:41:38 +0300

    first screen work

--- PetAdoption/ContentView.swift
+++ PetAdoption/ContentView.swift
@@ -14,94 +14,97 @@ struct ContentView: View {
     let ref = Database.database().reference()
     
     var body: some View {
-        VStack {
-            Spacer()
-            ZStack(alignment: .top) {
-                BottomBar(selected: self.$selected)
-                    .padding()
-                    .padding(.horizontal, 22)
-                    .background(CurvedShape())
+        
+        NavigationView {
+            
+            VStack {
                 
-                Button(action: {
-                    print("main image")
-                })  {
-                    Image(systemName: "hare").renderingMode(.original).padding(24)
-                }.background(Color.yellow)
-                    .clipShape(Circle())
-                    .offset(y: -35)
-                    .shadow(radius: 5)
+                Image("doggie")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: UIScreen.main.bounds.width - 10, height: UIScreen.main.bounds.height / 1.8)
+                    .cornerRadius(20)
+                    .padding(.top, 100)
                 
-            }.background(Color.white).edgesIgnoringSafeArea(.top)
-                
-                .onAppear() {
-                    self.ref.child("test/name").setValue("Kenny")
-                    self.ref.childByAutoId().setValue(["name":"Tom", "role":"Admin", "age": 30])
-            }
-        }.background(Color.black)
+                Spacer()
+                ZStack(alignment: .top) {
+                    BarButtons(selected: self.$selected)
+                        .padding()
+                        .padding(.horizontal, 22)
+                        .background(ButtomBar())
+                    
+                }.background(Color.white).edgesIgnoringSafeArea(.top)
+                    
+                    .onAppear() {
+                        //                    self.ref.child("test/name").setValue("Kenny")
+                        //                    self.ref.childByAutoId().setValue(["name":"Tom", "role":"Admin", "age": 30])
+                }
+            }.background(Color.white)
+                .navigationBarTitle("Doggo app", displayMode: .inline)
+                .navigationBarItems(leading: Image(systemName: "person"), trailing: Image("dog").resizable().frame(width: 30, height: 30))
+        }
     }
 }
+
+struct ButtomBar: View {
     
-    struct CurvedShape: View {
-        
-        var body : some View {
-            Path{ path in
-                path.move(to: CGPoint(x: 0, y: 0))
-                path.addLine(to: CGPoint(x: UIScreen.main.bounds.width, y: 0))
-                path.addLine(to: CGPoint(x: UIScreen.main.bounds.width, y: 55))
+    var body : some View {
+        Path{ path in
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLine(to: CGPoint(x: UIScreen.main.bounds.width, y: 0))
+            path.addLine(to: CGPoint(x: UIScreen.main.bounds.width, y: 55))
+            path.addLine(to: CGPoint(x: 0, y: 55))
+            
+        }.fill(Color.white)
+            .rotationEffect(.init(degrees: 180))
+    }
+}
+
+struct BarButtons : View {
+    
+    @Binding var selected: barItem
+    
+    var body : some View {
+        HStack {
+            Button(action: {
+                self.selected = .first
                 
-                path.addArc(center: CGPoint(x: UIScreen.main.bounds.width / 2, y: 55), radius: 30, startAngle: .zero, endAngle: Angle.init(degrees: 180), clockwise: true)
-                path.addLine(to: CGPoint(x: 0, y: 55))
+            }) {
+                Image(systemName: "info.circle")
+            }.foregroundColor(self.selected == .first ? .black : .gray)
+            
+            Spacer(minLength: 12)
+            
+            Button(action: {
+                self.selected = .second
                 
-            }.fill(Color.white)
-                .rotationEffect(.init(degrees: 180))
+            }) {
+                Image(systemName: "xmark")
+            }.foregroundColor(self.selected == .second ? .black : .gray)
+            
+            Spacer(minLength: 12)
+            
+            Button(action: {
+                self.selected = .third
+            }) {
+                Image(systemName: "suit.heart.fill")
+            }.foregroundColor(self.selected == .third ? .black : .gray)
+            
+            Spacer(minLength: 12)
+            
+            Button(action: {
+                self.selected = .forth
+            }) {
+                Image(systemName: "square.and.arrow.up")
+            }.foregroundColor(self.selected == .forth ? .black : .gray)
+            
+            
         }
     }
-    
-    struct BottomBar : View {
-        
-        @Binding var selected: barItem
-        
-        var body : some View {
-            HStack {
-                Button(action: {
-                    self.selected = .first
-                   
-                }) {
-                    Image(systemName: "desktopcomputer")
-                }.foregroundColor(self.selected == .first ? .black : .gray)
-                
-                Spacer(minLength: 12)
-                
-                Button(action: {
-                    self.selected = .second
-                  
-                }) {
-                    Image(systemName: "keyboard")
-                }.foregroundColor(self.selected == .second ? .black : .gray)
-                
-                Spacer().frame(width: 120)
-                
-                Button(action: {
-                    self.selected = .third
-                }) {
-                    Image(systemName: "person")
-                }.foregroundColor(self.selected == .third ? .black : .gray)
-                
-                Spacer(minLength: 12)
-                
-                Button(action: {
-                    self.selected = .forth
-                }) {
-                    Image(systemName: "person")
-                }.foregroundColor(self.selected == .forth ? .black : .gray)
-                
-               
-            }
-        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
 }
