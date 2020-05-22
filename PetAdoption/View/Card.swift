f6d06c8c7f93e11919c4ae55b75af60e169e6e35
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-05-23 00:18:26 +0300

    work

--- PetAdoption/View/Card.swift
+++ PetAdoption/View/Card.swift
@@ -1,9 +1,10 @@
 import SwiftUI
 
 struct Card: View {
- 
+    @State var displyed = 1
+    @State var imageCount = 5
     @ObservedObject var imageLoader: DataLoader
-    @State var image:UIImage = UIImage()
+    @State var image: UIImage = UIImage()
     
     
     init(imageURL: String) {
@@ -13,15 +14,29 @@ struct Card: View {
     var body: some View {
         ZStack(alignment: .bottomLeading) {
             VStack {
+
+                
                  Image(uiImage: image)
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .frame(width: UIScreen.main.bounds.width - 10, height: UIScreen.main.bounds.height / 1.8)
                     .cornerRadius(20)
-                    .padding(.top, 100)
                     .onReceive(imageLoader.didChange) { data in
                     self.image = UIImage(data: data) ?? UIImage()
                 }
+                HStack {
+                    ForEach (1...imageCount,id: \.self) { i in
+                        Rectangle()
+                            
+                            .fill(Color.clear)
+                            .background((self.displyed == i ? Color.black : Color.gray).cornerRadius(20))
+                            
+                            .frame(width: (UIScreen.main.bounds.width / CGFloat(self.imageCount)) - 30, height: 10)
+                    }
+     
+                    
+                        
+                }.padding(.top, -UIScreen.main.bounds.height / 1.8)
             }
             VStack(alignment: .leading, spacing: 12) {
                 Text("Doggie")
@@ -35,6 +50,6 @@ struct Card: View {
                     .fontWeight(.bold)
             }.padding(.bottom, 50)
                 .padding(.leading, 10)
-        }
+        }.padding(.top, 100)
     }
 }
