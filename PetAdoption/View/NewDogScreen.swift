55034795ec5a0333c938f18b182efe8ac8811e85
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-06-29 19:13:40 +0300

    fix

--- PetAdoption/View/NewDogScreen.swift
+++ PetAdoption/View/NewDogScreen.swift
@@ -2,13 +2,24 @@ import SwiftUI
 
 struct NewDogScreen: View {
     @Binding var isBarHidden: Bool
+    
+    init(isBarHidden: Binding<Bool>) {
+        self._isBarHidden = isBarHidden
+        UINavigationBar.appearance().backgroundColor =  UIColor.clear
+    }
+    
     var body: some View {
-        Text("hello")
-            .onAppear() {
-                self.isBarHidden = true
-        }
+        VStack {
+            Text("hello")
+                .onAppear() {
+                    self.isBarHidden = true
+            }
             .onDisappear() {
                 self.isBarHidden = false
-        }
+            }
+        }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height + 180)
+            .background(Color.offWhite)
+            .edgesIgnoringSafeArea(.top)
+            .edgesIgnoringSafeArea(.bottom)
     }
 }
