9eba1fd65786fb9d3460e59872e27b99b6583f49
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-06-29 20:47:15 +0300

    fix

--- PetAdoption/View/NewDogScreen.swift
+++ PetAdoption/View/NewDogScreen.swift
@@ -5,7 +5,12 @@ struct NewDogScreen: View {
     
     init(isBarHidden: Binding<Bool>) {
         self._isBarHidden = isBarHidden
-        UINavigationBar.appearance().backgroundColor =  UIColor.clear
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.titleTextAttributes = [.font : UIFont.systemFont(ofSize: 20), NSAttributedString.Key.foregroundColor : UIColor.black]
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().tintColor = .black
     }
     
     var body: some View {
@@ -18,8 +23,9 @@ struct NewDogScreen: View {
                 self.isBarHidden = false
             }
         }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height + 180)
-            .background(Color.offWhite)
-            .edgesIgnoringSafeArea(.top)
-            .edgesIgnoringSafeArea(.bottom)
+            .background(Color.offWhite.edgesIgnoringSafeArea([.all]))
+               .navigationBarTitle(Text("פרסום מודעה")
+                  .foregroundColor(.black)
+                  .font(.title), displayMode: .inline)
     }
 }
