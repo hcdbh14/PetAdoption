0cfd24cb4b9e06877bb0367cc4657a5168d2b636
kenny kurochkin <kennnkuro15@gmail.com>
2020-07-16 08:22:49 +0300

    fix

--- PetAdoption/View/MenuView.swift
+++ PetAdoption/View/MenuView.swift
@@ -63,6 +63,8 @@ struct MenuView: View {
                 }
                 Spacer()
             }.padding(.leading, -30)
+            
+            CheckboxField(id: "hey", label: "hello", size: 20, color: .white, textSize: 20, callback: checkboxSelected)
         }
         .frame(maxWidth: UIScreen.main.bounds.width / 1.2, maxHeight: .infinity, alignment: .topLeading)
         .background(Color("offBlack"))
@@ -74,5 +76,59 @@ private var line: some View {
     VStack {
         Divider()
             .background(Color.orange)
+    }
+}
+
+    func checkboxSelected(id: String, isMarked: Bool) {
+        print("\(id) is marked: \(isMarked)")
+    }
+
+
+import SwiftUI
+
+//MARK:- Checkbox Field
+struct CheckboxField: View {
+    let id: String
+    let label: String
+    let size: CGFloat
+    let color: Color
+    let textSize: Int
+    let callback: (String, Bool)->()
+    
+    init(
+        id: String,
+        label:String,
+        size: CGFloat = 10,
+        color: Color = Color.orange,
+        textSize: Int = 14,
+        callback: @escaping (String, Bool)->()
+        ) {
+        self.id = id
+        self.label = label
+        self.size = size
+        self.color = color
+        self.textSize = textSize
+        self.callback = callback
+    }
+    
+    @State var isMarked: Bool = false
+    
+    var body: some View {
+        Button(action:{
+            self.isMarked.toggle()
+            self.callback(self.id, self.isMarked)
+        }) {
+            HStack(alignment: .center, spacing: 10) {
+                Image(systemName: self.isMarked ? "checkmark.square" : "square")
+                    .resizable()
+                    .foregroundColor(Color.orange)
+                    .frame(width: self.size, height: self.size)
+                  
+                Text(label)
+                    .font(Font.system(size: size))
+                Spacer()
+            }.foregroundColor(self.color)
+        }
+        .foregroundColor(Color.white)
     }
 }
