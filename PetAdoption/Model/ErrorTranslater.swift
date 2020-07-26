3e3956b954e98332f17d8fb130e7b3f5fd78e908
kenny kurochkin <kennnkuro15@gmail.com>
2020-07-26 10:33:32 +0300

    work

--- PetAdoption/Model/ErrorTranslater.swift
+++ PetAdoption/Model/ErrorTranslater.swift
@@ -1,17 +1,17 @@
 import Foundation
 
 struct ErrorTranslater {
-
+    
     func signUpErrors(_ errorDescription: String) -> String {
         
         switch errorDescription {
-        
+            
         case "Network error (such as timeout, interrupted connection or unreachable host) has occurred.":
             return "לא נמצא חיבור לאינטרנט"
             
         case "The password must be 6 characters long or more.":
             return "סיסמה אמורה להיות 6 תווים לפחות"
-        
+            
         case "An email address must be provided.":
             return "חסר כתובת מייל"
             
@@ -30,10 +30,13 @@ struct ErrorTranslater {
     func signInErrors(_ errorDescription: String) -> String {
         
         switch errorDescription {
-        
+            
+        case "Network error (such as timeout, interrupted connection or unreachable host) has occurred.":
+            return "לא נמצא חיבור לאינטרנט"
+            
         case "The email address is badly formatted.":
             return "כתובת מייל שהוזן לא תקין"
-        
+            
         case "There is no user record corresponding to this identifier. The user may have been deleted.":
             return "סיסמה או מייל לא נכונים"
             
