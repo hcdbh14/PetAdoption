d1768af868ab31878c3267898e41493a65e3c7ad
kenny kurochkin <kennnkuro15@gmail.com>
2020-08-05 11:19:32 +0300

    work

--- PetAdoption/View/Regions.swift
+++ PetAdoption/View/Regions.swift
@@ -1,12 +1,56 @@
 import SwiftUI
 
 struct Regions: View {
+    
+    let regionsList = ["ראשון לצין","נתניה"]
     @Binding var showRegions: Bool
     
     init(showRegions: Binding<Bool>) {
         self._showRegions = showRegions
     }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: close) {
+                    HStack {
+                        Image(systemName: "chevron.right")
+                        Text("חזור")
+                    }
+                }
+                .foregroundColor(Color("orange"))
+                .padding(.leading, 25)
+                Spacer()
+            }
+            
+            List {
+                // 2.
+                Section(header:
+                Text("צפון")) {
+                    ForEach(0 ..< regionsList.count) {
+                        Text(self.regionsList[$0])
+                    }
+                }
+                // 3.
+                Section(header:
+                Text("מרכז")) {
+                    ForEach(0 ..< regionsList.count) {
+                        Text(self.regionsList[$0])
+                    }
+                }
+                
+                Section(header:
+                Text("דרום")) {
+                    ForEach(0 ..< regionsList.count) {
+                        Text(self.regionsList[$0])
+                    }
+                }
+                
+            }
+        }.environment(\.layoutDirection, .rightToLeft)
+    }
+    
+    func close() {
+        showRegions = false
     }
 }
