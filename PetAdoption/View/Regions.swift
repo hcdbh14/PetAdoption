278255e7ad592dc48c75876492b065e27e479bfb
kenny kurochkin <kennnkuro15@gmail.com>
2020-08-05 12:18:22 +0300

    work

--- PetAdoption/View/Regions.swift
+++ PetAdoption/View/Regions.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct Regions: View {
     
     let regionsList = ["ראשון לצין","נתניה"]
+    @State var searchText = ""
     @Binding var showRegions: Bool
     
     init(showRegions: Binding<Bool>) {
@@ -11,21 +12,36 @@ struct Regions: View {
     
     var body: some View {
         VStack {
-        HStack {
-            Button(action: close) {
-                HStack {
-                    Image(systemName: "chevron.right")
-                    Text("חזור")
+            HStack {
+                Button(action: close) {
+                    HStack {
+                        Image(systemName: "chevron.right")
+                        Text("חזור")
+                    }
                 }
+                .foregroundColor(Color("orange"))
+                .padding(.top, 15)
+                .padding(.leading, 25)
+                Spacer()
             }
-            .foregroundColor(Color("orange"))
-            .padding(.leading, 25)
-            Spacer()
-        }
-        
-        List(regionsList, id: \.self) { region in
-            Text(region)
-        }
+            
+            HStack {
+                Image(systemName: "magnifyingglass")
+                TextField("חיפוש עיר", text: $searchText)
+                Button(action: {
+                    self.searchText = ""
+                }) {
+                    Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
+                }
+            }.padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+                .foregroundColor(Color.gray)
+                .background(Color.white)
+                .cornerRadius(10.0)
+ 
+            List(self.regionsList.filter{$0.contains(self.searchText.lowercased()) || self.searchText == ""}, id: \.self) { region in
+                Text(region)
+            }
+            
         }.environment(\.layoutDirection, .rightToLeft)
     }
     
